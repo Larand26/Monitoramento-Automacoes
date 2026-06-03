51478da93900c129c0a137c2876ac2e4550c1a2e
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-06-03 14:01:24 -0300

    refactor: reloca para a home no caso de rota invalida

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import Home from "./pages/home/Home";
 import Dashboards from "./pages/dashboards/Dashboards";
@@ -11,6 +16,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboards" element={<Dashboards />} />
         <Route path="/logs" element={<Logs />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
