1ac6a8be4de43b8cc38bdfea63ab959d632c4d95
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-06-03 13:58:03 -0300

    refactor: ajusta nome de variavel tailwind

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col items-center">
       <NavBar pageActive="home" />
-      <div className="w-[90%] h-[calc(100vh-4rem)] p-4 margin-auto">
+      <div className="w-[90%] h-[calc(100vh-4rem)] p-4 m-auto">
         <h1 className="text-4xl font-bold text-primary">Home</h1>
         {/* statistics */}
         <div className="w-full  flex items-center justify-between gap-4 p-4">
